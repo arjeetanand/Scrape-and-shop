1ad81e325c2c2cdebee0277a9e49c3237221fa0f
Arjeet Anand <72692370+arjeetanand@users.noreply.github.com>
2023-07-14 22:40:35 +0530

    Update settings.py

--- shopping/settings.py
+++ shopping/settings.py
@@ -134,9 +134,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anandarjeet27@gmail.com'
-EMAIL_HOST_PASSWORD = 'ejjjmxqgxaxvuqlp'
+EMAIL_HOST_USER = 'YOUREMAIL@gmail.com'
+EMAIL_HOST_PASSWORD = 'YOURPASSWORD'
 
 
-KEY = 'rzp_test_2kQCjb0PB5yGAp'
-SECRET = 'nuLr6SKxpMDZMz4SHev4jaIS'
+KEY = 'YOURKEY'
+SECRET = 'YOUR SECRET CODE'
+
